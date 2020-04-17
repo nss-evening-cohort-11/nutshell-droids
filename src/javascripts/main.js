da8d670fa538991cafc341dac8d9715eaf101655
Sarah Holder <sr.holder@yahoo.com>
2020-04-17 01:54:37 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,13 +2,16 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 import myNavbar from './components/myNavbar/myNavBar';
 import authData from './helpers/data/authData';
+import hub from './components/hub/hub';
 import 'bootstrap';
+
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   myNavbar.printNavbar();
   authData.checkLoginStatus();
+  hub.printAirports();
 };
 
 init();
