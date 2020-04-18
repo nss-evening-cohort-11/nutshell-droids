74f0c1500f050e8a277bb2fc6b6178be08cea1eb
Kenny Mc <kenymc1@gmail.com>
2020-04-18 09:08:54 -0500

    finished read

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -5,12 +5,14 @@ const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in
+      $('#the-lounge').removeClass('hide');
       $('#navbar-logout-button').removeClass('hide');
       $('#google-auth').addClass('hide');
     } else {
       // person is NOT logged in
       $('#navbar-logout-button').addClass('hide');
       $('#google-auth').removeClass('hide');
+      $('#the-lounge').addClass('hide');
     }
   });
 };
